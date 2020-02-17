6467ee5250f4486eadb0c1c806cd70048b34c7a8
Jannas007 <jan-louis@qualica.com>
2020-02-17 09:22:09 +0100

    Updates

--- src/DbUp/DeploymentScripts/00 Configuratie.ExtraStoptijdProducts.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE IF NOT EXISTS Configuratie.ExtraStoptijdProducts
-(
-	ArtikelnummerMedea VARCHAR(10) NOT NULL PRIMARY KEY,
-	Productcode VARCHAR(10) NOT NULL,
-	ExtraStoptijdSeconden INT NOT NULL
-)
--- src/DbUp/DeploymentScripts/00 Dimensie.ObjectSources.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE IF NOT EXISTS Dimensie.ObjectSources
-(
-	ObjectSourceId INT NOT NULL PRIMARY KEY,
-	ObjectSource NVARCHAR(15) NOT NULL
-)
